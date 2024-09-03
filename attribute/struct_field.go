7811bd8e68abd15d1efb53d50183d048ac5ac273
linx <sulinke1133@gmail.com>
2024-09-03 16:01:53 +0800

    fix: no struct attr

--- attribute/struct_field.go
+++ attribute/struct_field.go
@@ -7,11 +7,21 @@ func GetFieldAttributeAsParamType(f *astp.Element) []*Attribute {
 	results := make([]*Attribute, 0)
 	if f.Item != nil {
 		attrs := GetStructAttrs(f.Item)
-		for _, attr := range attrs {
-			if attr.Type == TypeParam {
-				results = append(results, attr)
+		if len(attrs) <=0 {
+			attr := new(Attribute)
+			attr.Type = TypeInner
+			attr.Name = f.Name
+			attr.Value = ""
+			attr.Index = 0
+			results = append(results, attr)
+		} else {
+			for _, attr := range attrs {
+				if attr.Type == TypeParam {
+					results = append(results, attr)
+				}
 			}
 		}
+
 	} else {
 		attr := new(Attribute)
 		attr.Type = TypeInner
